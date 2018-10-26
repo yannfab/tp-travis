2016c91086e21a7c02676db7bf072789d025ce1f
Fablet Yann <yann.fablet.etu@univ-lille.fr>
2018-10-26 09:02:23 +0200

    cyka

--- lol.java
+++ lol.java
@@ -3,7 +3,7 @@ public class lol {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-	String blyat = 1;
+	String blyat = "1";
         System.out.println("lol");
     }
 
